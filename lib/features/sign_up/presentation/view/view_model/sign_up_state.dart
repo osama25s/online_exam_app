6a5b892f40ca5c2f7a9deeb38de7f730952e245d
mayaryasser2005 <mayaryasser300@gmail.com>
2025-12-06 20:58:42 +0200

    Delete unused files

--- lib/features/sign_up/presentation/view/view_model/sign_up_state.dart
+++ lib/features/sign_up/presentation/view/view_model/sign_up_state.dart
@@ -33,27 +33,3 @@ class SignUpState {
 
 
 
-
-
-
-
-
-
-
-// part of 'sign_up_cubit.dart';
-
-// abstract class SignUpState {}
-
-// class SignUpInitial extends SignUpState {}
-
-// class SignUpLoading extends SignUpState {}
-
-// class SignUpSuccess extends SignUpState {
-//   final SignUpResponseModel response;
-//   SignUpSuccess(this.response);
-// }
-
-// class SignUpError extends SignUpState {
-//   final String message;
-//   SignUpError(this.message);
-// }
